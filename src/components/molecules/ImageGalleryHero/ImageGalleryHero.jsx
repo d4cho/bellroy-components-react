27da9e848baa5696c1971bec318679011968a6f4
bioDanCho <daniel.cho@thebioagency.com>
2022-10-19 12:03:16 -0400

    image gallery hero

---
+++ src/components/molecules/ImageGalleryHero/ImageGalleryHero.jsx
@@ -0,0 +1,92 @@
+import React, { useState, useEffect, useMemo } from 'react';
+import './ImageGalleryHero.scss';
+import Button from '../../atoms/Button/Button';
+import { Link } from 'react-router-dom';
+
+const ImageGalleryHero = ({
+    leftImages,
+    rightImages,
+    heading,
+    ctaText,
+    ctaUrl,
+}) => {
+    const [currLeftIdx, setCurrLeftIdx] = useState(0);
+    const [currRightIdx, setCurrRightIdx] = useState(0);
+
+    const increaseLeft = useMemo(() => {
+        let startingIndex = 0;
+        setInterval(() => {
+            let newCurrIndex = startingIndex % leftImages.length;
+            setCurrLeftIdx(newCurrIndex);
+            startingIndex++;
+        }, 4000);
+    }, [leftImages.length]);
+
+    const increaseRight = useMemo(() => {
+        let startingIndex = 0;
+        setTimeout(() => {
+            setInterval(() => {
+                let newCurrIndex = startingIndex % rightImages.length;
+                setCurrRightIdx(newCurrIndex);
+                startingIndex++;
+            }, 4000);
+        }, 2000);
+    }, [rightImages.length]);
+
+    const shopButtonStyle = {
+        color: '#FFF',
+        backgroundColor: '#e15a1d',
+        border: 'none',
+        fontSize: '13px',
+        fontWeight: 'bold',
+        letterSpacing: '2px',
+        borderRadius: '4px',
+        cursor: 'pointer',
+    };
+
+    return (
+        <div className='ImageGalleryHero_container'>
+            {
+                leftImages.map((img, idx) => {
+                    return (
+                        <div
+                            key={'left' + idx}
+                            className='ImageGalleryHero_leftImage'
+                            style={{ backgroundImage: `url(${img})` }}
+                        ></div>
+                    );
+                })[currLeftIdx]
+            }
+            {
+                rightImages.map((img, idx) => {
+                    return (
+                        <div
+                            key={'right' + idx}
+                            className='ImageGalleryHero_rightImage'
+                            style={{ backgroundImage: `url(${img})` }}
+                        ></div>
+                    );
+                })[currRightIdx]
+            }
+
+            {/* <div
+                className='ImageGalleryHero_rightImage'
+                style={{ backgroundImage: `url(${rightImages[currRightIdx]})` }}
+            ></div> */}
+
+            <div className='ImageGalleryHero_content'>
+                <div className='ImageGalleryHero_heading'>{heading}</div>
+                <div className='ImageGalleryHero_cta'>
+                    <Link to={ctaUrl}>
+                        <Button
+                            content={ctaText}
+                            styleOverride={shopButtonStyle}
+                        />
+                    </Link>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default ImageGalleryHero;
--- src/components/views/ImageGalleryHeroPage/ImageGalleryHeroPage.jsx
+++ src/components/views/ImageGalleryHeroPage/ImageGalleryHeroPage.jsx
@@ -1,10 +1,30 @@
 import React from 'react';
+import ImageGalleryHero from '../../molecules/ImageGalleryHero/ImageGalleryHero';
 import './ImageGalleryHeroPage.scss';
 
+import leftImage1 from '../../../assets/images/HeroImages/Left/Tech-Homepage-MainBanner-Desktop-1A.avif';
+import leftImage2 from '../../../assets/images/HeroImages/Left/Tech-Homepage-MainBanner-Desktop-2A.avif';
+import leftImage3 from '../../../assets/images/HeroImages/Left/Tech-Homepage-MainBanner-Desktop-3A.avif';
+import leftImage4 from '../../../assets/images/HeroImages/Left/Tech-Homepage-MainBanner-Desktop-4A.avif';
+
+import rightImage1 from '../../../assets/images/HeroImages/Right/Tech-Homepage-MainBanner-Desktop-1B.avif';
+import rightImage2 from '../../../assets/images/HeroImages/Right/Tech-Homepage-MainBanner-Desktop-2B.avif';
+import rightImage3 from '../../../assets/images/HeroImages/Right/Tech-Homepage-MainBanner-Desktop-3B.avif';
+import rightImage4 from '../../../assets/images/HeroImages/Right/Tech-Homepage-MainBanner-Desktop-4B.avif';
+
 const ImageGalleryHeroPage = () => {
+    const leftImagesArr = [leftImage1, leftImage2, leftImage3, leftImage4];
+    const rightImagesArr = [rightImage1, rightImage2, rightImage3, rightImage4];
+
     return (
         <div className='ImageGalleryHeroPage_container'>
-            ImageGalleryHeroPage
+            <ImageGalleryHero
+                leftImages={leftImagesArr}
+                rightImages={rightImagesArr}
+                heading={"Designed for where you're going"}
+                ctaText={'SHOP TECH'}
+                ctaUrl={'/'}
+            />
         </div>
     );
 };
